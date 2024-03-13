234fa37c3a184248e79d72cd5efa270ff2d14b22
Dylan Runnels <runnels.dylan@yahoo.com>
2024-03-12 22:16:26 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, eventNames) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+  
+    return messages;
+  }
+  
+  let messagesArray = writeCards(['Guadalupe', 'Ollie', 'Aki']);
+  console.log(messagesArray);
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
